5e3230ffd8de69ae74681d45b75235a189467e5e
Dale Sande <dale.sande@gmail.com>
2018-10-23 12:19:55 -0700

    add baseline support for processing sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const StyleDictionary = require('style-dictionary').extend('./config.json');
 
 
@@ -6,3 +7,14 @@ const StyleDictionary = require('style-dictionary').extend('./config.json');
 gulp.task('buildTokens', function() {
   StyleDictionary.buildAllPlatforms();
 });
+
+gulp.task('buildCss', function() {
+  gulp.src('./scss/{,*/}*.{scss,sass}')
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'compressed', //alt options: nested, compact, compressed
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['buildTokens', 'buildCss']);
