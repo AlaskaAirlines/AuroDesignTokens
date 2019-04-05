8742fad95165d0d1a7ff1d77ca9e077cfaddd20f
Dale Sande <dale.sande@alaskaair.com>
2019-04-04 20:28:50 -0700

    wip

---
+++ scripts/styleDictionary.js
@@ -0,0 +1,5 @@
+// Required dependency
+const StyleDictionary = require('style-dictionary').extend('./scripts/config.json');
+
+// Style Dictionary build function
+StyleDictionary.buildAllPlatforms();
