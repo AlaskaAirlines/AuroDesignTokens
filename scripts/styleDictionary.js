15b4a79252ccede33dd606b7b57ca02f32ef1cee
Dale Sande <dale.sande@alaskaair.com>
2019-10-30 11:41:27 -0700

    build: update style dictionary for new pre-fab build types

--- scripts/styleDictionary.js
+++ scripts/styleDictionary.js
@@ -35,7 +35,4 @@ console.log('')
 const StyleDictionary = require('style-dictionary').extend('./scripts/config.json');
 
 // Style Dictionary build function
-StyleDictionary.buildPlatform('JavaScript');
-StyleDictionary.buildPlatform('SassVariables');
-StyleDictionary.buildPlatform('CSSCustomElements');
-StyleDictionary.buildPlatform('CSSCustomElements_SassFiletype');
+StyleDictionary.buildAllPlatforms();
