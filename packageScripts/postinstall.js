29ad15bae6fc39f43f8cd3b472db0a88231bfbcf
Dale Sande <dale.sande@alaskaair.com>
2020-05-16 11:01:19 -0700

    feat: support RGB custom color spec

---
+++ packageScripts/postinstall.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const chalk = require('chalk');
+const pjson = require('../package.json');
+
+console.log(chalk.hex('#f26135')(`
+ _______                   __           __ __
+|     __|.---.-.--.--.    |  |--.-----.|  |  |.-----.
+|__     ||  _  |  |  |    |     |  -__||  |  ||  _  |
+|_______||___._|___  |    |__|__|_____||__|__||_____|
+               |_____|
+ __              _______                    __
+|  |_.-----.    |   _   |.--.--.----.-----.|  |
+|   _|  _  |    |       ||  |  |   _|  _  ||__|
+|____|_____|    |___|___||_____|__| |_____||__|
+`)
++
+chalk.hex('#f26135')(`
+╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
+
+        Thanks for installing the latest version
+                of `) + chalk.hex('#ffd200').bold(`Design Tokens v${pjson.version}.`) + chalk.hex('#f26135')(`
+
+            Orion Tokens will continue to be
+                     FULLY SUPPORTED
+                until no longer required.
+
+╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
+`)
+);
--- scripts/styleDictionary.js
+++ scripts/styleDictionary.js
@@ -1,38 +1,24 @@
-console.log('')
-console.log('Build started...');
+const StyleDictionary = require('style-dictionary');
+const _ = require('lodash');
 
-console.log('')
-console.log('         .         . ')
-console.log('               *       *')
-console.log('')
-console.log('                 * * *')
-console.log('                    !')
-console.log('               *       * ')
-console.log('')
-console.log(" ██████╗ ██████╗ ██╗ ██████╗ ███╗   ██╗")
-console.log("██╔═══██╗██╔══██╗██║██╔═══██╗████╗  ██║")
-console.log("██║   ██║██████╔╝██║██║   ██║██╔██╗ ██║")
-console.log("██║   ██║██╔══██╗██║██║   ██║██║╚██╗██║")
-console.log("╚██████╔╝██║  ██║██║╚██████╔╝██║ ╚████║")
-console.log(" ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝")
-console.log('')
-console.log("██████╗ ███████╗███████╗██╗ ██████╗ ███╗   ██╗")
-console.log("██╔══██╗██╔════╝██╔════╝██║██╔════╝ ████╗  ██║")
-console.log("██║  ██║█████╗  ███████╗██║██║  ███╗██╔██╗ ██║")
-console.log("██║  ██║██╔══╝  ╚════██║██║██║   ██║██║╚██╗██║")
-console.log("██████╔╝███████╗███████║██║╚██████╔╝██║ ╚████║")
-console.log("╚═════╝ ╚══════╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝")
-console.log('')
-console.log("███████╗██╗   ██╗███████╗████████╗███████╗███╗   ███╗")
-console.log("██╔════╝╚██╗ ██╔╝██╔════╝╚══██╔══╝██╔════╝████╗ ████║")
-console.log("███████╗ ╚████╔╝ ███████╗   ██║   █████╗  ██╔████╔██║")
-console.log("╚════██║  ╚██╔╝  ╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║")
-console.log("███████║   ██║   ███████║   ██║   ███████╗██║ ╚═╝ ██║")
-console.log("╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝")
-console.log('')
+const Color = require('tinycolor2');
+
+StyleDictionary.registerTransform({
+  name: 'custom/color/rgb',
+  type: 'value',
+  matcher: function(prop) {
+    return prop.attributes.category === 'color';
+  },
+  transformer: function(prop) {
+    let newColor = Color(prop.value).toRgbString()
+    let reg = /\((.*)\)/;
+
+    return newColor.match(reg)[1];
+  }
+});
 
 // Required dependency
-const StyleDictionary = require('style-dictionary').extend('./scripts/config.json');
+const tokensConfig = StyleDictionary.extend('./scripts/config.json');
 
 // Style Dictionary build function
-StyleDictionary.buildAllPlatforms();
+tokensConfig.buildAllPlatforms();
