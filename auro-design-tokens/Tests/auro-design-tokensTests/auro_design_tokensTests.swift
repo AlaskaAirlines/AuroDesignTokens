a4aa79522d25a087b6455f672d89c790ecaae741
Alexander DiBenedetto [AS] <alexander.dibenedetto@alaskaair.com>
2025-03-14 13:31:52 -0700

    Delete unused files.

--- auro-design-tokens/Tests/auro-design-tokensTests/auro_design_tokensTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import auro_design_tokens
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
