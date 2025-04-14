48ae698b05365ab6e0fd02750e80a7d6e679469d
Alexander DiBenedetto [AS] <alexander.dibenedetto@alaskaair.com>
2025-04-14 14:31:38 -0700

    Rename to align to sdk naming standards.

--- Package.swift
+++ Package.swift
@@ -1,30 +1,37 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "auro-design-tokens",
+    name: "auroDesignTokens-sdk-ios",
     defaultLocalization: "en",
     platforms: [.macCatalyst(.v16), .iOS(.v16), .macOS(.v13), .watchOS(.v9), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "auro-design-tokens",
-            targets: ["auro-design-tokens"]),
+            name: "auroDesignTokens-sdk-ios",
+            targets: ["auroDesignTokens-sdk-ios"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "auro-design-tokens",
+            name: "auroDesignTokens-sdk-ios",
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ],
             linkerSettings: [
                 // Frameworks
                 .linkedFramework("UIKit")
             ]),
         .testTarget(
-            name: "auro-design-tokens-tests",
-            dependencies: ["auro-design-tokens"]
+            name: "auroDesignTokens-sdk-ios-tests",
+            dependencies: ["auroDesignTokens-sdk-ios"],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
