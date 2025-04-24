af5b60fa3722f0376c04d524605b2c57b6888c60
Alexander DiBenedetto [AS] <alexander.dibenedetto@alaskaair.com>
2025-04-23 17:34:52 -0700

    Add samples of generated files for review (will delete later).

---
+++ src/android/auroDesignTokens-sdk-android/src/main/kotlin/com/alaskaair/aurodesigntokens_sdk_android/TransparentColors.kt
@@ -0,0 +1,23 @@
+package com.alaskaair.aurodesigntokens_sdk_android
+
+import androidx.compose.ui.graphics.Color
+
+class TransparentColors {
+
+  val colorAlertBkgSuccessDefault = Color(0xffddf6e8)
+  val colorAlertBkgErrorDefault = Color(0xffffedf1)
+  val colorTierAlaskaMvpDefault = Color(0xff726e6c)
+  val colorTierAlaskaMvpInverse = Color(0xffc5c1bf)
+  val colorTierAlaskaMvpgoldDefault = Color(0xff7f682e)
+  val colorTierAlaskaMvpgoldInverse = Color(0xffc5c1bf)
+  val colorTierAlaskaMvpgold75kDefault = Color(0xff7f682e)
+  val colorTierAlaskaMvpgold75kInverse = Color(0xffc5c1bf)
+  val colorTierAlaskaMvpgold100kDefault = Color(0xff7f682e)
+  val colorTierAlaskaMvpgold100kInverse = Color(0xffc5c1bf)
+  val colorTierFareBusinessDefault = Color(0xff005154)
+  val colorTierFareFirstClassDefault = Color(0xff002c4e)
+  val colorTierOneworldEmerald = Color(0xff139142)
+  val colorTierOneworldSapphire = Color(0xff015daa)
+  val colorTierOneworldRuby = Color(0xffa41d4a)
+
+}
