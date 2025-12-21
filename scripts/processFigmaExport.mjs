3fc703edb0626e63fa30447c73937a51e3a77ba8
Jason Baker <jason@capsule42.com>
2025-12-20 19:05:09 -0800

    feat: build tokens from Figma Export #372

---
+++ scripts/processFigmaExport.mjs
@@ -0,0 +1,10 @@
+import FigmaExportUtil from "./figmaExportUtil.mjs";
+
+FigmaExportUtil.processTokenConfiguration('Alaska.tokens.json', 'web');
+FigmaExportUtil.processTokenConfiguration('Alaska.tokens.json', 'app');
+
+FigmaExportUtil.processTokenConfiguration('Hawaiian.tokens.json', 'web');
+FigmaExportUtil.processTokenConfiguration('Hawaiian.tokens.json', 'app');
+
+FigmaExportUtil.processTokenConfiguration('Atmos.tokens.json', 'web');
+FigmaExportUtil.processTokenConfiguration('Atmos.tokens.json', 'app');
