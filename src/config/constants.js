5bcf79858ed339f216e20a9b2a9d51bcd5e7933b
rmenner <rwmenner@gmail.com>
2026-03-06 18:01:53 -0500

    chore: move web theme definitions to the constants file

--- src/config/constants.js
+++ src/config/constants.js
@@ -13,3 +13,15 @@ export const CSS = {
   BUNDLED_FILE_NAME: 'CSSCustomProperties--bundled.css',
   ROOT_SELECTOR: ':root'
 };
+
+/**
+ * Web theme definitions for multi-theme selector rewriting in dist/web.
+ * Each entry maps a generated CSS filename to its data-aag-theme attribute code.
+ * These codes are intentionally separate from THEME_DEFINITIONS codes used in dist/themes.
+ * Add an entry here whenever a new web theme is added to outputConfigs/web/.
+ */
+export const WEB_THEME_DEFINITIONS = [
+  { file: 'alaska.css', code: 'aag-theme-as' },
+  { file: 'atmos.css', code: 'aag-theme-atm' },
+  { file: 'hawaiian.css', code: 'aag-theme-ha' },
+];
